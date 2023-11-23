22b882183b664cdb99102622716b3ace69da8cbf
Czechu77 <wojtek.czechu2@gmail.com>
2023-11-23 12:56:18 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//Scanner skanerr = new Scanner(System.in);
+//        System.out.println("Podaj wyraz");
+//        String str2 = skaner.nextLine();
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
